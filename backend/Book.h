8b39711a038e4df26ea1238b287c6d02a41430e2
byteMe <estherolatunji101@gmail.com>
2024-11-26 03:59:54 -0500

    Started setup

---
+++ backend/Book.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+// book information
+class Books{
+    string title;
+    string author;
+    int id;
+    string genre;
+    string date;
+    int avail_copies;
+    float rating;
+    string description;
+};
+
+class Library{
+
+};
+
